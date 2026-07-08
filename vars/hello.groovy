42d17919d2cd146779510fdc1c18cd9cb5c208e7
Rituraj-30 <rutu2930@gmail.com>
2026-07-08 09:39:13 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,5 @@
-def call(){
-  echo "Hello dosto"
+def call() {
+    echo "========================================="
+    echo "Hello Dosto! Shared Library Successfully Load "
+    echo "========================================="
 }
